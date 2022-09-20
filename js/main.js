90682d3537a4b916d11a2df520a958feb3cf8b8b
demamic <michael-elliott.demars@divtec.ch>
2022-09-20 15:11:37 +0200

    fini

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 /**
- * @author Steve Fallet
- * @since 2022.09.06
+ * @author Michaël Demars
+ * @since 2022.09.20
  */
 
-'use strict';
+const listeBackground = document.getElementById('background');
 
-console.log('Test liaison JS');
+listeBackground.addEventListener('change', function () {
+    const image = listeBackground.value
+    document.body.style.backgroundImage = `url(./img/backgrounds/${image})`
+})
+
+const formulaire = document.querySelector('form');
+const btCreer = document.querySelector('[type="submit"]');
+
+formulaire.addEventListener('submit', function (event) {
+    event.preventDefault();
+    btCreer.classList.add('animate__animated', 'animate__tada');
+})
+
+btCreer.addEventListener('animationend', function () {
+    alert('Compte créé avec succès !');
+    formulaire.reset();
+    btCreer.classList.remove('animate__animated', 'animate__tada');
+});
